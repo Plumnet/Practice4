2db459957c07a00e0bec85cfbdb643f056ab6547
Plumnet <tomomi0131@ymail.ne.jp>
2023-11-05 15:18:00 +0900

    Hooksを利用してレンダリングできるようになったソース

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,12 +1,13 @@
+import { useState } from "react";
 import "./App.css"
 
 function App() {
-let count = 0;
+  const [count, setCount] = useState(0);
+
 
 const  handleClick = () => {
-  count++;
-  console.log(count);
-}
+  setCount(count + 1);
+};
 
   return (
     <div className="App">
