64d3c6e03d38f8e26bafc7ae11a4e6876b94cb32
Plumnet <tomomi0131@ymail.ne.jp>
2023-11-06 17:05:23 +0900

    コンポーネントのソース

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
-import "./App.css"
+import { useEffect, useState, useContext } from "react";
+import "./App.css";
+import ShinCodeContext from "./main";
 
 function App() {
   const [count, setCount] = useState(0);
+  const shincodeInfo = useContext(ShinCodeContext);
 
 
 const  handleClick = () => {
@@ -11,14 +13,19 @@ const  handleClick = () => {
 
 useEffect(() => {
   console.log("Hello Hooks");
-  //setCount(count + 1);
+  //　setCount(count + 1);
 }, [count]);
 
   return (
     <div className="App">
-      <h1>UseState, UseEffect</h1>
+      <h1>useState, useEffect</h1>
       <button onClick={handleClick}>+</button>
       <p>{count}</p>
+
+      <hr />
+      <h1>useCotext</h1>
+      <p>{shincodeInfo.name}</p>
+      <p>{shincodeInfo.age}</p>
     </div>
     );
 }
