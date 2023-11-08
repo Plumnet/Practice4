83adc73818f82ba9df7ae67b3e4113c79d44cc10
Plumnet <tomomi0131@ymail.ne.jp>
2023-11-08 21:11:56 +0900

    useReducerのソース

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,11 +1,23 @@
-import { useEffect, useState, useContext , useRef} from "react";
+import { useEffect, useState, useContext , useRef, useReducer} from "react";
 import "./App.css";
 import ShinCodeContext from "./main";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function App() {
   const [count, setCount] = useState(0);
   const shincodeInfo = useContext(ShinCodeContext);
   const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
 
 const  handleClick = () => {
@@ -37,6 +49,12 @@ const handleRef = () => {
       <h1>useRef</h1>
       <input type="text" ref={ref} />
       <button onClick={handleRef}>UseRef</button>
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>カウント:{state}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "increment" })}>-</button>
     </div>
     );
 }
